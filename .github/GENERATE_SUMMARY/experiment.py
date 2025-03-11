06595877b86fbb043f3697127f9d8af60a3ae65e
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-03-11 13:08:19 +0000

    Generating simplified json for CMIP7 experiments.
    Remember to remove experiment and sub-experiment contexts as not to create infinite loops
    
    experiment simplified
    
    experiment simplified
    
    issue processing framework transfer
    
    development
    
    development
    
    experiment key and description correction
    
    Automated update of JSONLD files, DO NOT EDIT.
    
    updating the actions
    
    updating the actions
    
    Automated Content Update
    
    development
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    New entry imaginary-1 in experiment files.
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    New entry imaginary-1 in experiment files.
    
    development
    
    Automated Content Update
    
    Delete CMIP7_CVs_experiment_simplified.json
    Automated Content Update
    
    Delete CMIP7_CVs_experiment.json
    Automated Content Update
    
    Delete src-data/experiment/imaginary-1.json
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    New entry piClim-aer in experiment files.
    
    Delete src-data/experiment/piclim-aer.json
    Delete src-data/experiment/_schema_
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    development
    
    Automated Content Update
    
    Update cmipld_experiment.yml
    Automated Content Update
    
    Update experiment.py
    Automated Content Update
    
    Update experiment.py
    Automated Content Update
    
    Update experiment.py
    Automated Content Update
    
    instruction update to cmipld_experiment.yml
    Automated Content Update
    
    Update to instructions cmipld_experiment.yml
    Automated Content Update
    
    development
    
    scenariomip id correction
    
    Automated update of JSONLD files, DO NOT EDIT.
    
    Automated Content Update

---
+++ .github/GENERATE_SUMMARY/experiment.py
@@ -0,0 +1,25 @@
+import cmipld
+from cmipld.utils.ldparse import *
+from cmipld.utils.checksum import version
+
+me = __file__.split('/')[-1].replace('.py','')
+
+def run(localhost,whoami,repopath,reponame):
+
+    url = f'{localhost}/{whoami}/{me}/graph.jsonld'
+    ctx = f'{localhost}/{whoami}/{me}/_context_'
+    
+    data = cmipld.jsonld.frame(url,ctx)["@graph"]
+    
+    summary = name_extract(data)
+    
+    location = f'{repopath}/{reponame}_{me}.json'
+    summary = version(summary, me, location.split("/")[-1])
+    
+    if os.path.exists(location):
+        old = cmipld.utils.io.rjson(location)
+        if old['Header']['checksum'] == summary['Header']['checksum']:
+            return 'no update - file already exists'
+    
+    # cmipld.utils.io.wjsn(summary,location)
+    return location,me,summary
