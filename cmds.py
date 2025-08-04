bf65390611a6fab6c7d10115d392d3e11201472b
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-08-04 23:35:25 +0100

    documentation update and actions

--- cmds.py
+++
@@ -1,4 +0,0 @@
-python /Users/daniel.ellis/WIPwork/CMIP-LD/copier/mkdocs/auto-setup.py --no-confirm --force-overwrite
-
-mkdocs build -f .src/mkdocs/mkdocs.yml 
-mkdocs serve -f .src/mkdocs/mkdocs.yml
