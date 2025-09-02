79f25be1dc34aed686ab70f4400d650ce6e4fe98
alaupin-ipsl <alain.laupin-vinatier@ipsl.fr>
2025-09-02 15:03:48 +0200

    feat : Add overriding fields on experiment CV terms for all fields updated exclusively for CMIP7

--- scripts/create_experiment.py
+++ scripts/create_experiment.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pathlib import Path
+from datetime import datetime
 
 import esgvoc.api as ev
 import requests
@@ -22,11 +23,64 @@ def fetch_json(url):
     return response.json()
 
 
+def get_parent_activity_id(value: str, experiment_dir: str = "experiment"):
+    if value == "none":
+        return [None]
+
+    project_root = Path.cwd().parent
+    experiment_dir = project_root / "WCRP-universe" / "experiment"
+    parent_file = os.path.join(experiment_dir, f"{value}.json")
+
+    if not os.path.exists(parent_file):
+        print(f"Parent file for {value} not found : {parent_file}")
+        return [None]
+
+    with open(parent_file, "r", encoding="utf-8") as f:
+        parent_data = json.load(f)
+
+    return parent_data.get("activity_id")
+
+
+def normalize_experiment_data(experiment_data):
+    print(experiment_data)
+    start_year = experiment_data.get('start-date', experiment_data.get('start_year', ''))
+    end_year = experiment_data.get('end', experiment_data.get('end_year', ''))
+
+    if not isinstance(start_year, int):
+        try:
+            if start_year.lower() != "none":
+                start_year = datetime.fromisoformat(start_year).year
+        except ValueError:
+            start_year = None
+    try:
+        end_year = int(end_year)
+    except ValueError:
+        end_year = None  # or handle as needed if conversion fails
+    
+    model_realms = experiment_data.get('model-realms', [])
+    if isinstance(model_realms, dict):
+        model_realms = [model_realms]
+
+    return {
+        'activity_id': experiment_data.get('activity', []),
+        'additional_allowed_model_components': model_realms,
+        'description': experiment_data.get('description', ''),
+        'end_year':end_year,
+        'experiment': experiment_data.get('ui-label', ''),
+        'experiment_id': experiment_data.get('validation-key', ''),
+        'min_number_yrs_per_sim': experiment_data.get('minimum-number-of-years') if (experiment_data.get('minimum-number-of-years') != "none" and experiment_data.get('minimum-number-of-years') != "") else None,
+        'parent_experiment_id': experiment_data.get('parent-experiment', []),
+        'start_year': start_year,
+        'sub_experiment_id': experiment_data.get('sub_experiment_id', []),
+        'tier': experiment_data.get('tier', '')
+    }
+
+
 data = fetch_json(json_url)["experiment"]
 
 known_sources_in_universe = ev.get_all_terms_in_data_descriptor("experiment")
-print(known_sources_in_universe)
-for item in data:
+# print(known_sources_in_universe)
+for item, content in data.items():
     found_item = None
     for experiment in known_sources_in_universe:
         if experiment.drs_name == item:
@@ -36,11 +90,21 @@ for item in data:
     if found_item is None:
         print(item, "not found in universe")
     else:
+        normalized_data = normalize_experiment_data(content)
         # Create json file
         dict_to_save = {
             "@context": "000_context.jsonld",
             "id": found_item.id,
             "type": found_item.type,
+            "additional_allowed_model_components": [v.get('id').split('/')[-1].lower() for v in normalized_data['additional_allowed_model_components']],
+            "description": normalized_data["description"],
+            "end_year": int(normalized_data['start_year']) + int(normalized_data['min_number_yrs_per_sim']) - 1 if (normalized_data['start_year'] and normalized_data['min_number_yrs_per_sim'] and normalized_data['start_year'] != "none") else normalized_data['end_year'],
+            "experiment": normalized_data['experiment'],
+            "min_number_yrs_per_sim": normalized_data['min_number_yrs_per_sim'] if (normalized_data['min_number_yrs_per_sim'] != "none" and normalized_data['min_number_yrs_per_sim']!="" ) else None,
+            'parent_activity_id': get_parent_activity_id(normalized_data['parent_experiment_id'].split('/')[-1].lower()),
+            'parent_experiment_id': [normalized_data['parent_experiment_id'].split('/')[-1].lower() if normalized_data['parent_experiment_id'].split('/')[-1].lower() != "none" else None],
+            'required_model_components': [v.get('id').split('/')[-1].lower() for v in normalized_data['additional_allowed_model_components'] if v.get("is-required")],
+            "tier": normalized_data["tier"]
         }
         # print(dict_to_save)
         with open(Path(save_dir) / f"{found_item.id}.json", "w") as f:
