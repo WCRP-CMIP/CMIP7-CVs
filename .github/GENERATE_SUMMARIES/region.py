c15a742a9429c3f831eab8a8ac1c030b859bc788
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-11-05 14:40:57 +0000

    project files for cmip7

---
+++ .github/GENERATE_SUMMARIES/region.py
@@ -0,0 +1,10 @@
+import cmipld
+from cmipld.utils.ldparse import name_entry
+me = __file__.split('/')[-1].replace('.py','')
+def run(io, whoami, path, name, **kwargs):
+    data = cmipld.get(f'cmip7:project/{me}.json', depth=2)[me]
+    summary = name_entry(data)
+    
+    
+    # update the name to use the id field
+    return f"{path}/{name}_{me}.json", me, summary
