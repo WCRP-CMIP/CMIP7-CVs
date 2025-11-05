1eccc88435f3db7f75402121e7167af166b8dc59
cmip-ipo <actions@wcrp-cmip.org>
2025-11-05 14:41:12 +0000

    Clean file upload from src-data

---
+++ .github/GENERATE_SUMMARIES/license.py
@@ -0,0 +1,13 @@
+import cmipld
+from cmipld.utils.ldparse import key_extract, name_entry
+me = __file__.split('/')[-1].replace('.py','')
+def run(io, whoami, path, name, **kwargs):
+    data = cmipld.get(f'cmip7:project/{me}.json', depth=2)[me]
+    
+    
+    
+    summary = key_extract(data,['description','regex'])
+    
+    
+    # update the name to use the id field
+    return f"{path}/{name}_{me}.json", me, summary
