80e2fc87e5e5919bc179de2a23436cc374390b61
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-11-19 16:35:45 +0000

    automation script - graph key is now redundant

--- .github/GENERATE_SUMMARY/experiment_simplified.py
+++ .github/GENERATE_SUMMARY/experiment_simplified.py
@@ -4,7 +4,7 @@ from cmipld.utils.ldparse import name_extract, key_extract, name_entry
 me = __file__.split('/')[-1].replace('.py','')
 
 def run(io, whoami, path, name, **kwargs):
-    data = cmipld.get(f"{io}/experiment/graph.jsonld", depth=1)["@graph"]
+    data = cmipld.get(f"{io}/experiment/graph.jsonld", depth=1)
     full = name_extract(data)
     # summary = {k: key_extract(v, ['ui-label', 'description']) for k, v in full.items()}
     summary = name_entry(data, 'ui-label')
