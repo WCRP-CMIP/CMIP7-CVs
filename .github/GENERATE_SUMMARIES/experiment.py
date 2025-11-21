6d2b205e8ed713463df1ae0480ae02befa9f50ea
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-11-21 11:05:57 +0000

    label types and test source entries

--- .github/GENERATE_SUMMARIES/experiment.py
+++ .github/GENERATE_SUMMARIES/experiment.py
@@ -11,20 +11,15 @@ print(graph)
 
 
 def ld2cmip6(validation_key, ui_label, description, activity, 
-                 parent_experiment, tier, model_realms, 
+                 parent_experiment, tier, model_realm_required,model_realm_additional_allowed, 
                  start="", end="", min_years=0, parent_activity=None, **kwargs):
     """Convert form data to experiment JSON format."""
 
-    model_realms = model_realms if isinstance(model_realms, list) else [model_realms]
-
-    required = [m for m in model_realms if m.get('is_required')]
-    optional = [m for m in model_realms if not m.get('is_required')]
-    
 
     return {
         validation_key: {
             "activity_id": cvjson_validation_key(activity),
-            "additional_allowed_model_components": cvjson_validation_key(optional),
+            "additional_allowed_model_components": cvjson_validation_key(model_realm_additional_allowed),
             "description": description,
             # "end": end,
             "experiment": ui_label,
@@ -32,7 +27,7 @@ def ld2cmip6(validation_key, ui_label, description, activity,
             # "min_number_yrs_per_sim": min_years,
             "parent_activity_id": cvjson_validation_key(parent_activity) if parent_activity else [],
             "parent_experiment_id": cvjson_validation_key(parent_experiment),
-            "required_model_components":cvjson_validation_key(required),
+            "required_model_components":cvjson_validation_key(model_realm_required),
             # "start": start,
             # "tier": tier
         }
@@ -44,7 +39,7 @@ def run(io, whoami, path, name, **kwargs):
     data = cmipld.get('cmip7:experiment/graph.jsonld', depth=2)
 
     summary = {}
-    for i in data['@graph']:
+    for i in data:
         summary.update(ld2cmip6(**i))
         
     summary = dict(sorted(summary.items()))
--- .github/GENERATE_SUMMARIES/frequency.py
+++ .github/GENERATE_SUMMARIES/frequency.py
@@ -1,6 +1,7 @@
 import cmipld
 from cmipld.utils.ldparse import name_entry
 me = __file__.split('/')[-1].replace('.py','')
+
 def run(io, whoami, path, name, **kwargs):
     data = cmipld.get(f'cmip7:project/{me}.json', depth=2)[me]
     summary = name_entry(data)
