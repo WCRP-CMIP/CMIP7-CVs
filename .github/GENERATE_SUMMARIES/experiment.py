388bd94716892484a6d40f43503da26485650362
cmip-ipo <actions@wcrp-cmip.org>
2025-12-01 03:02:22 +0000

    Clean file upload from src-data

--- .github/GENERATE_SUMMARIES/experiment.py
+++ .github/GENERATE_SUMMARIES/experiment.py
@@ -11,7 +11,7 @@ print(graph)
 
 
 def ld2cmip6(validation_key, ui_label, description, activity, 
-                 parent_experiment, tier, model_realm_required,model_realm_additional_allowed, 
+                 parent_experiment, tier, model_realms_required,model_realms_additional_allowed, 
                  start="", end="", min_years=0, parent_activity=None, **kwargs):
     """Convert form data to experiment JSON format."""
 
@@ -19,7 +19,7 @@ def ld2cmip6(validation_key, ui_label, description, activity,
     return {
         validation_key: {
             "activity_id": cvjson_validation_key(activity),
-            "additional_allowed_model_components": cvjson_validation_key(model_realm_additional_allowed),
+            "additional_allowed_model_components": cvjson_validation_key(model_realms_additional_allowed),
             "description": description,
             # "end": end,
             "experiment": ui_label,
@@ -27,7 +27,7 @@ def ld2cmip6(validation_key, ui_label, description, activity,
             # "min_number_yrs_per_sim": min_years,
             "parent_activity_id": cvjson_validation_key(parent_activity) if parent_activity else [],
             "parent_experiment_id": cvjson_validation_key(parent_experiment),
-            "required_model_components":cvjson_validation_key(model_realm_required),
+            "required_model_components":cvjson_validation_key(model_realms_required),
             # "start": start,
             # "tier": tier
         }
@@ -39,10 +39,20 @@ def run(io, whoami, path, name, **kwargs):
     data = cmipld.get('cmip7:experiment/graph.jsonld', depth=2)
 
     summary = {}
+    failed = []
     for i in data:
-        summary.update(ld2cmip6(**i))
+        try:
+            summary.update(ld2cmip6(**i))
+        except Exception as e:
+            print(f"Error processing item {i.keys()}: {e}" )
+            failed.append(i)
         
+
+    for i in failed:
+        print(f"Failed item: {i['@id']}")
+
     summary = dict(sorted(summary.items()))
+    
         
     # update the name to use the id field
     me2 = me + '_id'
