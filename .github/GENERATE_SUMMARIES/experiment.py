4eb17280738890a330bc96f6831608b02b88b0a4
cmip-ipo <actions@wcrp-cmip.org>
2025-12-01 16:09:59 +0000

    Clean file upload from src-data

--- .github/GENERATE_SUMMARIES/experiment.py
+++ .github/GENERATE_SUMMARIES/experiment.py
@@ -1,3 +1,4 @@
+from glob import glob
 import cmipld,os
 from cmipld.utils.ldparse import cvjson_validation_key
 me = __file__.split('/')[-1].replace('.py','')
@@ -36,23 +37,44 @@ def ld2cmip6(validation_key, ui_label, description, activity,
     
 
 def run(io, whoami, path, name, **kwargs):
-    data = cmipld.get('cmip7:experiment/graph.jsonld', depth=2)
+    data = cmipld.get('cmip7:experiment/graph.jsonld', depth=1)
 
     summary = {}
     failed = []
+
+    nokeys =[]
+
     for i in data:
         try:
+            i = cmipld.get(f"cmip7:experiment/{i.get('@id')}.json", depth=2)
+        except Exception as e:
+            print(f"Error fetching data for {i.get('@id')}: {e}")
+            failed.append(i)
+            continue
+    # for i in glob('experiment/*.json'):
+    #     i = cmipld.get(f"file://{os.path.abspath(i)}", depth=2)
+
+
+
+        try:
+            
             summary.update(ld2cmip6(**i))
         except Exception as e:
             print(f"Error processing item {i.keys()}: {e}" )
             failed.append(i)
+            nokeys.append(i)
         
 
     for i in failed:
         print(f"Failed item: {i['@id']}")
 
+
     summary = dict(sorted(summary.items()))
     
+
+    for i in nokeys:
+        print(f"Missing: {i['@id']},{i}")
+
         
     # update the name to use the id field
     me2 = me + '_id'
